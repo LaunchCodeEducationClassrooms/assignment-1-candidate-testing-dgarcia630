4b05f30ca2a2bc4c3cf39fbd3d7e4b1ac06e5496
Donna Garcia <garciafamily63012@gmail.com>
2021-02-19 01:25:32 +0000

    Second try

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,5 +1,5 @@
 const input = require('readline-sync');
-//not needed-let info = input.question('Please enter your name.');
+
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
@@ -30,44 +30,43 @@ candidateName = input.question("\nEnter name please:\n")
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  for (let i = 0; i++ < questions.length; i++) {
+  for (let i = 0; i < questions.length; i++) {
     candidateAnswers[i] = input.question(questions[i]);
   }
-//if (candidateAnswer===correctAnswer) {
- // console.log ("Great job!");
-//} else if (candidateAnswer != correctAnswer) {
- // console.log ("Sorry, this answer is incorrect.")
-//}
+if (candidateAnswer===correctAnswer) {
+  console.log ("Great job!");
+} else (candidateAnswer != correctAnswer); {
+  console.log ("Sorry, this answer is incorrect.")
+}
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-console.log ("Hello"+(candidateName)); //changed variable
+console.log ('\NCandidate Name:${candidateName}');
 let answersCorrect = 0;
 console.log(`\nCandidate Name: ${candidateName}`);
 for (let i = 0; i < questions.length; i++) {
   if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
 answersCorrect++
   }
-console.log(`${i+1}) ${questions[i]}\n Canidate answer: 
-     ${candidateAnswers[i]} Correct answer: ${correctAnswers[i]}`+"\n");
+console.log(`${i+1}) ${questions[i]}\n Candidate answer: 
+     ${candidateAnswers[i]}\n Correct answer: ${correctAnswers[i]}`+"\n");
 }
   let grade = (answersCorrect/questions.length)*100;
-  console.log('>>>Overall Grade: ${grade}% (${answersCorrect} of ${questions.length} correct answers>>>');
+  console.log('>>>Overall Grade: ${grade}% (${answersCorrect} of ${questions.length} correct answers.>>>');
   if (grade = 100) {
-    console.log(">>> Overall Grade: 100% (5 of 5 responses correct) <<<\nStatus: PASSED")
-  }
-  else if (grade > 80) {
-    console.log(">>> Overall Grade: 80% (4 of 5 responses correct)<<<\n Status: PASSED <<< ");
+  console.log(">>> Overall Grade: 100% (5 of 5 responses correct) <<<\nStatus: PASSED")
+  } else if (grade > 80) {
+  console.log(">>> Overall Grade: 80% (4 of 5 responses correct)<<<\n Status: PASSED <<< ");
   } else if (grade = 60) {
-    console.log(">>> Overall Grade: 60% (3 of 5 responses correct)<<< \n>>>Status: FAILED");
-  } else if (grade = 40) {
-    console.log(">>>Overall Grade: 40% (2 of 5 responses correct)<<< \n >>>Status: Failed");
-  } else if (grade = 20) {
-    console.log(">>>Overall Grade: 20% (1 of 5 responses correct)<<< \n >>>Status: FAILED");
-    if (grade=0) {
-      console.log(">>>Overall Grade: 0% (0 of 5 responses correct)<<< \n >>>Status: FAILED");
-    }
+  console.log(">>> Overall Grade: 60% (3 of 5 responses correct)<<< \n>>>Status: FAILED");
+  }else if (grade = 40) {
+  console.log(">>>Overall Grade: 40% (2 of 5 responses correct)<<< \n >>>Status: Failed");
+  }else if (grade = 20) {
+  console.log(">>>Overall Grade: 20% (1 of 5 responses correct)>>> \n >>>Status: FAILED");
+  if (grade=0) {
+  console.log(">>>Overall Grade: 0% (0 of 5 responses correct)<<< \n >>>Status: FAILED");
+  }
   }
 
   return grade;
